d5b696f876d12a50989b44a83bb742f786cf5431
Paul <Paul2708@users.noreply.github.com>
2024-12-23 09:41:42 +0100

    Refactor solution

--- Day-23/python/paul2708/day23.py
+++ Day-23/python/paul2708/day23.py
@@ -1,15 +1,4 @@
-import math
-from typing import Tuple, List, Set
-
-import tqdm
-
-from shared.paul2708.input_reader import *
-from shared.paul2708.output import write
-
-import math
-from typing import Tuple, List, Set
-
-import tqdm
+from typing import Collection
 
 from shared.paul2708.input_reader import *
 from shared.paul2708.output import write
@@ -18,57 +7,44 @@ lines = read_plain_input(day=23, example=None)
 links = [tuple(line.split("-")) for line in lines]
 computers = set([a for a, b in links] + [b for a, b in links])
 
-
-def is_connected_to(from_node, to_node):
-    for a, b in links:
-        if from_node == a and to_node == b or from_node == b and to_node == a:
-            return True
-
-    return False
-
-
-connections = []
-
-for a, b in tqdm.tqdm(links):
-    for c in computers:
-        if is_connected_to(a, c) and is_connected_to(b, c):
-            if {a, b, c} in connections:
-                continue
-
-            connections.append({a, b, c})
-
-res = 0
-for nodes in tqdm.tqdm(connections):
-    if any(map(lambda n: n.startswith("t"), nodes)):
-        res += 1
-
-print(res)
-
-lines = read_plain_input(day=23, example=None)
-links = [tuple(line.split("-")) for line in lines]
-computers = set([a for a, b in links] + [b for a, b in links])
-
-connected_to = dict()
+connected_to = set()
 
 for a, b in links:
-    connected_to[(a, b)] = True
-    connected_to[(b, a)] = True
+    connected_to.add((a, b))
+    connected_to.add((b, a))
 
 
-def is_connected_to(from_node, to_node):
+def is_connected_to(from_node: str, to_node: str) -> bool:
     return (from_node, to_node) in connected_to
 
 
-def is_connected_to_all(from_nodes, to_node):
-    for f in from_nodes:
-        if not is_connected_to(f, to_node):
+def is_connected_to_all(from_nodes: Collection[str], to_node: str) -> bool:
+    for from_node in from_nodes:
+        if not is_connected_to(from_node, to_node):
             return False
 
     return True
 
 
+# Part 1
+connections = []
+
+for a, b in links:
+    for c in computers:
+        if is_connected_to(a, c) and is_connected_to(b, c):
+            connections.append({a, b, c})
+
+matches = 0
+for nodes in connections:
+    if any(map(lambda node: node.startswith("t"), nodes)):
+        matches += 1
+
+write(f"<{matches // 3}> triple connections contain at least one computer <starting with t>.")
+
+# Part 2
 max_links = []
-for a, b in tqdm.tqdm(links):
+
+for a, b in links:
     curr_links = {a, b}
     curr_len = 0
 
@@ -82,5 +58,5 @@ for a, b in tqdm.tqdm(links):
     if len(curr_links) > len(max_links):
         max_links = curr_links
 
-
-print(",".join(sorted(max_links)))
+password = ",".join(sorted(max_links))
+write(f"The password is <{password}>.")
