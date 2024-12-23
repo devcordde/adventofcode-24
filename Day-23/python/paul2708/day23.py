c0893339e8d6aa61d97096d116ecff8f0101bac2
Paul <Paul2708@users.noreply.github.com>
2024-12-23 09:25:06 +0100

    Solve part 2

--- Day-23/python/paul2708/day23.py
+++ Day-23/python/paul2708/day23.py
@@ -6,6 +6,14 @@ import tqdm
 from shared.paul2708.input_reader import *
 from shared.paul2708.output import write
 
+import math
+from typing import Tuple, List, Set
+
+import tqdm
+
+from shared.paul2708.input_reader import *
+from shared.paul2708.output import write
+
 lines = read_plain_input(day=23, example=None)
 links = [tuple(line.split("-")) for line in lines]
 computers = set([a for a, b in links] + [b for a, b in links])
@@ -34,6 +42,45 @@ for nodes in tqdm.tqdm(connections):
     if any(map(lambda n: n.startswith("t"), nodes)):
         res += 1
 
-print(connections)
-print(len(connections))
 print(res)
+
+lines = read_plain_input(day=23, example=None)
+links = [tuple(line.split("-")) for line in lines]
+computers = set([a for a, b in links] + [b for a, b in links])
+
+connected_to = dict()
+
+for a, b in links:
+    connected_to[(a, b)] = True
+    connected_to[(b, a)] = True
+
+
+def is_connected_to(from_node, to_node):
+    return (from_node, to_node) in connected_to
+
+
+def is_connected_to_all(from_nodes, to_node):
+    for f in from_nodes:
+        if not is_connected_to(f, to_node):
+            return False
+
+    return True
+
+
+max_links = []
+for a, b in tqdm.tqdm(links):
+    curr_links = {a, b}
+    curr_len = 0
+
+    while curr_len != len(curr_links):
+        curr_len = len(curr_links)
+
+        for c in computers:
+            if is_connected_to_all(curr_links, c):
+                curr_links.add(c)
+
+    if len(curr_links) > len(max_links):
+        max_links = curr_links
+
+
+print(",".join(sorted(max_links)))
